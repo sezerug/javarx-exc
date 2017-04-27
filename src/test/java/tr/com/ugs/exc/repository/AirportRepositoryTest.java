6a685746b1ed5ccc2059585ca6478636ff43ed63
sezerug <ug.sezer@gmail.com>
2017-04-27 17:52:46 +0300

    test

--- src/test/java/tr/com/ugs/exc/repository/AirportRepositoryTest.java
+++ src/test/java/tr/com/ugs/exc/repository/AirportRepositoryTest.java
@@ -3,8 +3,11 @@ package tr.com.ugs.exc.repository;
 
 import org.junit.Before;
 import org.junit.Test;
+import rx.observables.GroupedObservable;
 import tr.com.ugs.exc.domain.Airport;
+import tr.com.ugs.exc.domain.Country;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -34,5 +37,30 @@ public class AirportRepositoryTest {
         int countByIsoCountry = 119;
         List<Airport> airports = repository.getData().filter(airport -> airport.getIsoCountry().equals(isoCountry)).toList().toBlocking().first();
         assertEquals(countByIsoCountry, airports.size());
+    }
+
+    @Test
+    public void groupByIsoCountry() {
+        repository.getData().filter(airport -> airport.getIsoCountry().startsWith("T")).groupBy(Airport::getIsoCountry)
+                .flatMap(groups -> groups.collect(Bucket::new, (bucket, rows) -> {
+                    bucket.name = rows.getIsoCountry();
+                    bucket.airports.add(rows);
+                })).sorted((bucket1, bucket2) -> Integer.compare(bucket1.airports.size(), bucket2.airports.size()))
+                .subscribe(System.out::println);
+    }
+
+    public static class Bucket {
+        public String name;
+        public List<Airport> airports = new ArrayList<>();
+
+        @Override
+        public String toString() {
+            String str = " (" + name + ") [ ";
+            for (Airport airport : airports) {
+                str += (airport.toString() + " ");
+            }
+            str += "]";
+            return str;
+        }
     }
 }
